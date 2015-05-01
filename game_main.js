8741426ea85478161c109f7bf6ba85c0d9ce2991
Liang Huang <huangliang01@Liangs-MacBook-Pro.local>
2015-05-01 13:54:08 +0800

    update

--- game_main.js
+++ game_main.js
@@ -101,15 +101,15 @@ GameMain.prototype.pauseGame = function () {
 GameMain.prototype.endGame = function () {
 	this.game_state = "GAME_END";
 }
-GameMain.prototype.throwOneCard=function(card){
-	var index=this.card_list.indexOf(card);
-	if(index!=-1){
-		table.addCard(this.roomId,this.card_list[index]);
-		this.card_list.splice(index,1);
+GameMain.prototype.throwOneCard = function (card) {
+	var index = this.card_list.indexOf(card);
+	if (index != -1) {
+		table.addCard(this.roomId, this.card_list[index]);
+		this.card_list.splice(index, 1);
 	}
 }
-GameMain.prototype.getOneCard=function(card){
-	var card=mj_list.dealOneCard(this.roomId);
+GameMain.prototype.getOneCard = function (card) {
+	var card = mj_list.dealOneCard(this.roomId);
 	this.list.push(card);
 }
 GameMain.prototype.chi = function (card_one, card_two) {
@@ -117,9 +117,9 @@ GameMain.prototype.chi = function (card_one, card_two) {
 	var match_arr = [match_card, card_one, card_two];
 	var type_arr = this.getTypeByMatchArr(match_arr);
 	var num_arr = this.getNumsByMatchArr(match_arr);
-	
+
 	var same_type = this.checkArrIsEqual(type_arr);
-	
+
 	var be_list = true;
 	var item = null;
 	num_arr.sort();
@@ -134,96 +134,172 @@ GameMain.prototype.chi = function (card_one, card_two) {
 	}
 	if (same_type && be_list) {
 		//符合吃的条件
-//		global.io_obj.to(this.roomId).emit('chi',{card:match_card});
+		//		global.io_obj.to(this.roomId).emit('chi',{card:match_card});
 	} else {
-		global.io_obj.to(this.roomId).emit('not able chi',{error:'不满足条件'});
+		global.io_obj.to(this.roomId).emit('not able chi', { error: '不满足条件' });
 	}
 }
-GameMain.prototype.peng = function (card_one,card_two) {
-	var match_card=talbe.lastCard();
-	var match_arr=[match_card,card_one,card_two];
-	var type_arr=this.getTypeByMatchArr(match_arr);
-	var num_arr=this.getNumsByMatchArr(match_arr);
-	
-	var same_type=this.checkArrIsEqual(type_arr);
-	var same_num=this.checkArrIsEqual(num_arr);
-	
-	if(same_num&&same_type){
+GameMain.prototype.peng = function (card_one, card_two) {
+	var match_card = talbe.lastCard();
+	var match_arr = [match_card, card_one, card_two];
+	var type_arr = this.getTypeByMatchArr(match_arr);
+	var num_arr = this.getNumsByMatchArr(match_arr);
+
+	var same_type = this.checkArrIsEqual(type_arr);
+	var same_num = this.checkArrIsEqual(num_arr);
+
+	if (same_num && same_type) {
 		//满足碰的条件
-	}else{
-		global.io_obj.to(this.roomId).emit('not able peng',{error:'不满足条件'});
+	} else {
+		global.io_obj.to(this.roomId).emit('not able peng', { error: '不满足条件' });
 	}
 }
 
-GameMain.prototype.gang = function (card_one,card_two,card_three) {
-	var match_card=table.lastCard();
-	var match_arr=[match_card,card_one,card_two,card_three];
-	var type_arr=this.getTypeByMatchArr(match_arr);
-	var num_arr=this.getNumsByMatchArr(match_arr);
-	
-	var same_type=this.checkArrIsEqual(type_arr);
-	var same_num=this.checkArrIsEqual(num_arr);
-	
-	
-	
-	if(same_num&&same_type||this.check_gane()){
+GameMain.prototype.gang = function (card_one, card_two, card_three) {
+	var match_card = table.lastCard();
+	var match_arr = [match_card, card_one, card_two, card_three];
+	var type_arr = this.getTypeByMatchArr(match_arr);
+	var num_arr = this.getNumsByMatchArr(match_arr);
+
+	var same_type = this.checkArrIsEqual(type_arr);
+	var same_num = this.checkArrIsEqual(num_arr);
+
+
+
+	if (same_num && same_type || this.check_gane()) {
 		//
-	}else{
-		global.io_obj.to(this.roomId).emit('not able gane',{error:'不满足条件'});
+	} else {
+		global.io_obj.to(this.roomId).emit('not able gane', { error: '不满足条件' });
 	}
 }
-GameMain.prototype.check_gang=funciton(){
-	var _check={};
-	var _indexArr=[];
-	this.list.forEach(function(val,index){
-		if(_check[val]==undefined){
-			_check[val]=new Array();
+GameMain.prototype.check_gang = funciton(){
+	var _check = {};
+	var _indexArr = [];
+	this.card_list.forEach(function (val, index) {
+		if (_check[val] == undefined) {
+			_check[val] = new Array();
 			_indexArr.push(_check[val]);
-		}else{
+		} else {
 			_check[val].push(index);
 		}
 	});
-	_indexArr.forEach(function(val){
-		if(val.length==4){
+	_indexArr.forEach(function (val) {
+		if (val.length == 4) {
 			return true;
 		}
 	})
 	return false;
 }
 GameMain.prototype.hu = function () {
-	var 
+
 }
-GameMain.prototype.check_jiang=function(){
-	
+GameMain.prototype.check_hu = function () {
+	var wan_arr = this.getTypesByList('wan');
+	var feng_arr = this.getTypesByList('f');
+	var o_arr = this.getTypesByList('o');
+	var tiao_arr = this.getTypesByList('tiao');
+	var tong_arr = this.getTypesByList('tong');
+
+	var wan_info = this.getInfoByList(wan_arr);
 }
-GameMain.prototype.check_juzi=function(){
-	
+GameMain.prototype.getTypesArrByList = function (type) {
+	var arr = [];
+	this.card_list.forEach(function (val, index) {
+		var tp = val.split('_')[1];
+		if (tp == type) {
+			arr.push(val);
+		}
+	});
+	return arr;
 }
-GameMain.prototype.check_shunzi=function(){
-	
+GameMain.prototype.getInfoByList = function (list) {
+	var obj = {};
+	if (list.length) {
+		obj['duizi'] = this.check_duizi(list);
+		obj['juzi'] = this.check_juzi(list);
+
+		if (list[0].split('_')[1] != 'o' || list[0].split('_')[1] != 'f') {
+			obj['shunzi'] = this.check_shunzi(list);
+		}
+	}
+	return obj;
 }
-GameMain.prototype.checkArrIsEqual=function(array){
-	var same=true;
+GameMain.prototype.check_duizi = function (array) {
+	var dui_arr = [];
+
+	if (array.length >= 2) {
+		array.sort();
+		for (var i = 0; i < array.length - 1; i++) {
+			var ele = array[i];
+			var ele1 = array[i + 1];
+			if (ele == ele1) {
+				dui_arr.push([ele, ele]);
+				i++;
+			}
+		}
+	}
+
+	return dui_arr;
+}
+GameMain.prototype.check_juzi = function (array) {
+	var juzi_arr = [];
+	if (array.length >= 3) {
+		array.sort();
+		for (var index = 0; index < array.length - 2; index++) {
+			var element = array[index];
+			var element1 = array[index + 1];
+			var element2 = array[index + 2];
+			if (element == element1 == element2) {
+				juzi_arr.push([element, element, element]);
+				index += 2;
+			}
+		}
+	}
+
+	return juzi_arr;
+}
+GameMain.prototype.check_shunzi = function (array) {
+	var shunzi_arr = [];
+	var nums_arr = [];
+	if (array.length >= 3) {
+		array.sort();
+		array.forEach(function (val, index) {
+			nums_arr.push(val.split('_')[0]);
+		});
+		for (var index = 0; index < nums_arr.length; index++) {
+			var element = nums_arr[index];
+			var element1 = nums_arr[index + 1];
+			var element2 = nums_arr[index + 2];
+			if (element1 == (element + 1) && element2 == (element1 + 1)) {
+				index += 2;
+				shunzi_arr.push([element, element1, element2]);
+			}
+		}
+	}
+	return shunzi_arr;
+}
+GameMain.prototype.checkArrIsEqual = function (array) {
+	var same = true;
 	for (var index = 0; index < array.length; index++) {
 		var element = array[index];
-		if(element!=array[0]){
-			same=false;
+		if (element != array[0]) {
+			same = false;
 		}
 	}
 	return same;
 }
-GameMain.prototype.getTypeByMatchArr=function(array){
-	var arr=[];
-	if(Object.prototype.toString.call(array)==="[object Array]"){
-		array.forEach(function(val){
+GameMain.prototype.getTypeByMatchArr = function (array) {
+	var arr = [];
+	if (Object.prototype.toString.call(array) === "[object Array]") {
+		array.forEach(function (val) {
 			arr.push(val.split('_')[1]);
 		})
 	}
 }
-GameMain.prototype.getNumsByMatchArr=function(array){
-	var arr=[];
-	if(Object.prototype.toString.call(array)==="[object Array]"){
-		array.forEach(function(val){
+GameMain.prototype.getNumsByMatchArr = function (array) {
+	var arr = [];
+	if (Object.prototype.toString.call(array) === "[object Array]") {
+		array.forEach(function (val) {
 			arr.push(val.split('_')[0]);
 		})
 	}
